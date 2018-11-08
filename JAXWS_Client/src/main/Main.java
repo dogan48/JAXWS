0016016633c82add527ce2d6735723259d0c0ed7
Dogan <hadidogankocabiyik@gmail.com>
2018-11-08 10:27:33 +0300

    Client eklendi

---
+++ JAXWS_Client/src/main/Main.java
@@ -0,0 +1,35 @@
+package main;
+
+import ws.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		try {
+			
+			ProductWSImplService productImplService = new ProductWSImplServiceLocator();
+			ProductWS productWS = productImplService.getProductWSImplPort();
+			
+			//System.out.println("Id: " + productWS.find().getId());
+			//System.out.println("Name: " + productWS.find().getName());
+			//System.out.println("Price: " + productWS.find().getPrice());
+			Product []listProduct = productWS.findAll();
+			for(Product p: listProduct) {
+				
+				System.out.println("Id: " + p.getId());
+				System.out.println("Name: " + p.getName());
+				System.out.println("Price: " + p.getPrice());
+				
+				System.out.println("=================================");
+			}
+			
+			
+		} catch (Exception e) {
+
+			System.out.println(e.getMessage());
+		}
+
+	}
+
+}
